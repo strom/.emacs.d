9261d64b335a0be5f0f6c41212b80795f2a5cbd0
Strom Lee <stroml@apple.com>
2017-05-08 14:55:35 -0700

    Put auto-save in the home dir

--- init.el
+++ init.el
@@ -247,7 +247,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-save-default nil)
+ '(auto-save-file-name-transforms (quote (("." "~/.autosaves" nil))))
  '(backup-directory-alist (quote (("." . "~/.backups"))))
  '(column-number-mode t)
  '(company-backends
