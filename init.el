12ec347e685c27bd28c17739ab9ebca09de1f6af
Strom Lee <stroml@apple.com>
2017-05-08 14:57:40 -0700

    Put autosaves and backups in the emacs dir instead

--- init.el
+++ init.el
@@ -247,8 +247,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-save-file-name-transforms (quote (("." "~/.autosaves" nil))))
- '(backup-directory-alist (quote (("." . "~/.backups"))))
+ '(auto-save-file-name-transforms (quote (("." "~/.emacs.d/.autosaves" nil))))
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/.backups"))))
  '(column-number-mode t)
  '(company-backends
    (quote
