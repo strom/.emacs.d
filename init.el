bc1fe348a267d5daa2da62d4adc814b6a32c02ce
Strom Lee <stroml@apple.com>
2017-05-11 11:27:06 -0700

    One more attempt at autosave dir

--- init.el
+++ init.el
@@ -264,7 +264,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-save-file-name-transforms (quote (("." "~/.emacs.d/.autosaves/" nil))))
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/.autosaves/" t))))
  '(backup-directory-alist (quote (("." . "~/.emacs.d/.backups"))))
  '(column-number-mode t)
  '(company-backends
